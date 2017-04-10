0a51a20c47e01fdfc11ece230d3666d801ba6996
Ziad Ahmed <z.ahmed@live.com>
2017-04-10 23:36:38 +0000

    requestURL returns the correct url as expected

--- download-avatars.js
+++ download-avatars.js
@@ -2,9 +2,20 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "YOUR USERNAME HERE";
+var GITHUB_TOKEN = "YOUR ACCESSTOKEN HERE";
+
+//Get contributors function
+//Takes 3 parameters: Owner, Name, Callback Function
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+
 }
+
+//Caling the function with hardcoded parameters
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
